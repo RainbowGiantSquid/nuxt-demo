73b1781daa7dbadc3d861675bbcf51d06f0d9477
Dagmara <dagmarakukla@gmail.com>
2022-02-25 00:32:24 +0000

    feat: nuxt basic layout

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    I am a layout
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout'
+}
+</script>
---
+++ layouts/main.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex">
+    {{ title }}
+    <div class="flex-1">
+      <Nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainLayout',
+
+  data () {
+    return {
+      title: 'I am a title'
+    }
+  }
+}
+</script>
